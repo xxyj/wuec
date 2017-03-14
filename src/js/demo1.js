35fd8dfbd8ad51c0a4b5d5a18c25b52089f246b9
hzxiongxu <xx@xiongxudeMacBook-Pro.local>
2017-03-14 14:41:28 +0800

    code update

--- src/js/demo1.js
+++ src/js/demo1.js
@@ -7,7 +7,7 @@ export default{
       list: {
         "0": "核心公式:A.x + A.w >= B.x && B.x + B.w >= A.x && A.y + A.h >= B.y && B.y + B.h >= A.y",
         "1": "核心公式:Math.sqrt(Math.pow(A.x - B.x, 2) + Math.pow(A.y - B.y, 2)) <= A.r + B.r",
-        "2": "核心公式:A.x-=B.radius;A.y-=B.radius,A.width+=2*B.radius,A.height+=B.radius;ctx.isPointInpath(B.R(x,y))",
+        "2": "核心思路:判断圆形是否与四边碰撞或则矩形的四个角是否在园内即可",
       },
       type: "0",
       //用来存储碰撞的物体
@@ -24,11 +24,11 @@ export default{
         {x: 160, y: 30, r: 5, sx: -0.8, sy: -1.2}
       ],
       mix: [
-        {x: 100, y: 120, r: 10, sx: 1, sy: 1},
-        {x: 50, y: 50, w: 20, h: 40, sx: 0.5, sy: 0},
-        {x: 210, y: 90, r: 8, sx: -1.1, sy: 0},
-        {x: 160, y: 30, r: 5, sx: -0.8, sy: -1.2},
-        {x: 250, y: 20, w: 20, h: 20, sx: -0.8, sy: -1.2},
+        {x: 100, y: 120, r: 10, sx: 0.5, sy: 0.5},
+        {x: 50, y: 50, w: 20, h: 40, sx: 0.25, sy: 0},
+        {x: 210, y: 90, r: 8, sx: -0.6, sy: 0},
+        {x: 160, y: 30, r: 5, sx: -0.6, sy: -0.6},
+        {x: 250, y: 20, w: 20, h: 20, sx: -0.4, sy: 0.6},
       ],
       color: ["#ff00ff", "#0000ff", "#3cd088", "#0bbdef", "#bbb333", "#sea318", "#014321"]
     }
@@ -124,35 +124,43 @@ export default{
       }
     },
     /**
+     * 检测圆形A和矩形B是否碰撞
+     * @param A
+     * @param B
+     */
+    raTore: function (A, B) {
+      let x = Math.max(B.x - A.r, 0);
+      let y = Math.max(B.y - A.r, 0);
+      let w = Math.min(B.x + B.w + A.r, this.canvas.width);
+      let h = Math.min(B.y + B.h + A.r, this.canvas.height);
+      //分别生成2个矩形,判断远点是否在2个矩形之内
+      let isInrect1 = A.x >= x && A.x <= w && A.y >= B.y && A.y <= B.y + B.h;
+      let isInrect2 = A.x >= B.x && A.x <= B.x + B.w && A.y >= y && A.y <= h;
+      //计算4个点是否在园内
+      let b1 = Math.pow(B.x - A.x) + Math.pow(B.y - B.y) <= A.r * A.r;
+      let b2 = Math.pow(B.x + B.w - A.x) + Math.pow(B.y - B.y) <= A.r * A.r;
+      let b3 = Math.pow(B.x - A.x) + Math.pow(B.y + B.h - B.y) <= A.r * A.r;
+      let b4 = Math.pow(B.x + B.w - A.x) + Math.pow(B.y + B.h - B.y) <= A.r * A.r;
+      if (isInrect1 || isInrect2 || b1 || b2 || b3 || b4) {
+        [A.sy, B.sy] = [B.sy, A.sy];
+        [A.sx, B.sx] = [B.sx, A.sx];
+      }
+    },
+    /**
      * 模块之间的碰撞  方与方  圆与圆  方与园
      * @param A
      * @param B
      */
     collestionOther: function (A, B) {
       if (A.r && B.r) {
-        if (A.x + A.w >= B.x && B.x + B.w >= A.x && A.y + A.h >= B.y && B.y + B.h >= A.y) {
+        if (Math.sqrt(Math.pow(A.x - B.x, 2) + Math.pow(A.y - B.y, 2)) <= A.r + B.r) {
           [A.sy, B.sy] = [B.sy, A.sy];
           [A.sx, B.sx] = [B.sx, A.sx];
         }
       } else if (A.r) {
-        let x = Math.max(B.x - A.r, 0);
-        let y = Math.max(B.y - A.r, 0);
-        let w = Math.min(B.x + B.w + A.r, this.canvas.width);
-        let h = Math.min(B.y + B.h + A.r, this.canvas.height);
-        if (A.x >= x && A.x <=  w && A.y >= y && A.y <= h) {
-          [A.sy, B.sy] = [B.sy, A.sy];
-          [A.sx, B.sx] = [B.sx, A.sx];
-        }
-
+        this.raTore(A, B);
       } else if (B.r) {
-        let x = Math.max(A.x - B.r, 0);
-        let y = Math.max(A.y - B.r, 0);
-        let w = Math.min(A.x + A.w + B.r, this.canvas.width);
-        let h = Math.min(A.y + A.h + B.r, this.canvas.height);
-        if (B.x >= x && B.x <=  w && B.y >= y && B.y <= h) {
-          [A.sy, B.sy] = [B.sy, A.sy];
-          [A.sx, B.sx] = [B.sx, A.sx];
-        }
+        this.raTore(B, A);
       }
       else {
         if (A.x + A.w >= B.x && B.x + B.w >= A.x && A.y + A.h >= B.y && B.y + B.h >= A.y) {
