e7035fb34b71f0c99f7f36edaa5f8c626ac26058
hzxiongxu <xx@xiongxudeMacBook-Pro.local>
2017-03-21 14:46:41 +0800

    update

--- src/components/demo4.vue
+++ src/components/demo4.vue
@@ -2,7 +2,7 @@
   <div id="demo4" style="padding:30px;">
     <p>打雷了,下雨了,回家收衣服了!!!</p>
 
-    <p>关键函数:<a href="http://www.w3school.com.cn/tags/canvas_ispointinpath.asp" target="_blank">isPointInPath</p>
+    <p>关键函数:<a href="http://www.w3school.com.cn/tags/canvas_ispointinpath.asp" target="_blank">isPointInPath</a></p>
     <canvas ref="canvas4" style="background: #000;width:400px;height:450px;"></canvas>
   </div>
 </template>
