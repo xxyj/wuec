55bab46418a26fce905cca2e4eaec5e43c75e7b5
hzxiongxu <xx@xiongxudeMacBook-Pro.local>
2017-03-21 14:36:41 +0800

    update

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,8 +1,14 @@
 <template>
   <div style="padding:50px;">
-    介绍下常用的几种二维环境下碰撞检测的方法<br>
-    用于技术分享,所以代码很多没有考虑兼容性<br>
-    by hzxiongxu
+    <p>介绍下常用的几种canvas碰撞检测的方法</p>
+    <p>仅仅用于技术分享</p>
+    <p>by hzxiongxu</p>
+    <p>demo地址:https://github.com/xxyj/wuec</p>
+    <p>使用步骤:</p>
+    <p>1.下载代码: git clone https://github.com/xxyj/wuec.git</p>
+    <p>2.进入目录下安装依赖: npm install</p>
+    <p>3.运行: npm run dev</p>
+
   </div>
 
 </template>
